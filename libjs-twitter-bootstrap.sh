afc5058b8c9503b7e8480a2283fcc3e0fb7037bf
Vítězslav Dvořák <vitex@murka.cz>
2014-09-03 00:34:13 +0200

    Initial Commit

---
+++ libjs-twitter-bootstrap.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [ ! -d "bootstrap-switch" ]; then
+    git clone https://github.com/nostalgiaz/bootstrap-switch.git
+    cd bootstrap-switch 
+else
+    cd bootstrap-switch 
+    git pull
+fi
+
+VERSION=`cat package.json | grep '"version":' | awk -F '"'  '{ print $4 }'`
+#CHANGES=`cat CHANGELOG.md | awk -vRS="##" 'NR<=2' ORS="##" | grep -v "##"`
+#tar -czf ~/Projects/Packaging/libjs-twitter-bootstrap/libjs-twitter-bootstrap_$VERSION.orig.tar.gz .
+#dch -v $VERSION-1 --package libjs-twitter-bootstrap $CHANGES
+
+cd ..
+
+dch -v $VERSION-5 --package libjs-twitter-bootstrap-switch 
+
+debuild -i -us -uc -b
+
+cd ..
+#~/bin/publish-deb-packages.sh
