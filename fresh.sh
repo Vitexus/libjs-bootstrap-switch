8d921dc4d33f3ce6d547df4c6d9aaec86e6c7afa
Vítězslav Dvořák <info@vitexsoftware.cz>
2021-09-14 00:33:10 +0200

    Refresh js code befofe packae build

--- fresh.sh
+++ fresh.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ ! -d "bootstrap-switch" ]; then
-    git clone https://github.com/Bttstrp/bootstrap-switch.git
+    git clone --depth=1 https://github.com/Bttstrp/bootstrap-switch.git
     cd bootstrap-switch 
 else
     cd bootstrap-switch 
